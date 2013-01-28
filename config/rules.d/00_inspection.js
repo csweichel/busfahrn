45cb163b1b4ca59f2a7bece2a5c243d3ce8a1848
32leaves <csweichel@googlemail.com>
2013-01-28 16:40:03 +0000

    users can now peek on the bus (listen for exactly one message), bus now supports all registration methods as described in the documentation, inspection module puts messages back on the bus instead of the console, allowing users to lookup messages without explicit Redis IO access

--- config/rules.d/00_inspection.js
+++ config/rules.d/00_inspection.js
@@ -3,7 +3,7 @@ module.exports = function(inference, bus, redis) {
     
     inference.rule("inspection_latest").on("inspection.latest").then(function(msgtype, time, msg, proceeding) {
         redis.latest(msg.type, function(err, res) {
-            console.log(JSON.stringify(res));
+            bus.post("inspection.latest." + msg.type, res);
         }, msg.limit || 1);
     }).enforce();
     
