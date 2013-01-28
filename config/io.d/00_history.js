2569997460ffcf33043952c5402630a491d34cbe
32leaves <csweichel@googlemail.com>
2013-01-28 16:41:03 +0000

    default history IO configuration ignores inspection messages to reduce the load on Redis

--- config/io.d/00_history.js
+++ config/io.d/00_history.js
@@ -3,7 +3,9 @@ var io_redis = require(__lib + 'io_redis.js');
 
 module.exports = function(bus, modules) {
     return new io_redis(bus)(function(io) {
-        io.listen("_all");
+        io.listen(function(type, time, msg) {
+            return type.indexOf('inspection') === -1;
+        });
     });
 };
 
