10f75c9e480ecae16a92af482fd4bedda61f1f0f
32leaves <csweichel@googlemail.com>
2013-02-01 01:26:04 +0000

    IO_Redis now supports message type based storage/retrieval strategies, default history configuration supports status and time messages

--- config/io.d/00_history.js
+++ config/io.d/00_history.js
@@ -2,10 +2,17 @@
 var io_redis = require(__lib + 'io_redis.js');
 
 module.exports = function(bus, modules) {
+    // configuration for the redis module:
+    //   - captured using a strategy: none, default, status and timeseries messages
+    //   - has to support storage and retrieval
+    
     return new io_redis(bus)(function(io) {
-        io.listen(function(type, time, msg) {
-            return type.indexOf('inspection') === -1;
-        });
+        io.listen("_all", [
+            [ /inspection.*/ , io_redis.Strategy.none      ],
+            [ /status.*/     , io_redis.Strategy.status    ],
+            [ /time.*/       , io_redis.Strategy.timeseries],
+        ]);
     });
+    
 };
 
