5b1d05cdec7755bb628659c53464766b07b7ecec
32leaves <csweichel@googlemail.com>
2013-02-01 00:37:45 +0000

    httpserver now also sends the timestamp of a message, listen_message script supports a filter function to ease command line scripting

--- scripts/listen_message.js
+++ scripts/listen_message.js
@@ -5,7 +5,13 @@ var crypto  = require('crypto'),
     argv    = require('optimist').argv;
     
 function printUsage() {
-    console.log("usage: listen_message -h|--host <host> -u|--user <user> -p|--password <pwd> -t|--type <msgtype>");
+    console.error("usage: listen_message -h|--host <host> -u|--user <user> -p|--password <pwd> -t|--type <msgtype> [-o|--once <function>]");
+    console.error("\t-h|--host <host>   \ta host address, including http/https and port if neccesary");
+    console.error("\t-u|--user <user>   \tthe username to login with");
+    console.error("\t-p|--password <pwd>\tthe users password");
+    console.error("\t-t|--type <msgtype>\ta message type to listen for, can also be _all");
+    console.error("\t[-o|--once <function>]\toptional - if used, the Javascript function given is used to determine when to terminate the program");
+    
     process.exit(-1);
 }
 
@@ -13,11 +19,29 @@ var host = argv.host     || argv.h || printUsage();
 var user = argv.user     || argv.u || printUsage();
 var pass = argv.password || argv.p || printUsage();
 var type = argv.type     || argv.t || printUsage();
+var once = argv.once     || argv.o || "function(type, time, msg) { return false; }";
     
 var timestamp = Math.floor(Date.now() / 1000);
 var hash = crypto.createHmac("sha1", pass).update(timestamp + type).digest("hex");
 var url  = host + "/" + user + "/" + hash + "/" + timestamp + "/" + type;
 
+var codeWellRunThroughEval = "var shouldExit = " + once;
+try {
+    eval(codeWellRunThroughEval);
+} catch(e) {
+    console.error("Error while evaluating the once function:\t\n" + codeWellRunThroughEval + "\n" + e);
+    process.exit(-1);
+}
+
 console.log("listening on " + url);
 
-request(url).pipe(process.stdout);
+//request(url).pipe(process.stdout);
+request(url).on('data', function(data) {
+    console.log(data.toString());
+    if(data.toString().trim().length > 0) {
+        var msg = JSON.parse(data);
+        if(shouldExit(msg.type, msg.time, msg.msg)) {
+            process.exit(0);
+        }
+    }
+});
