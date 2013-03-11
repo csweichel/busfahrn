70b6e511d09b2fe696afa5ac8e8d29c4ab333a7b
32leaves <csweichel@googlemail.com>
2013-03-11 21:50:05 +0000

    Made the TCP haltestelle support more robust / dropped native support for UDP

---
+++ scripts/native_clients/config/senselamp_v00.h
@@ -0,0 +1,12 @@
+/**
+ * The following line is important. It tells the build script what implementation to use.
+ * Ensure it has exactly one whitespace character between the definition and the filename.
+ */
+#define IMPLEMENTATION senseless.c
+
+
+#define USE_SERIAL
+//define SERIAL_BAUDRATE B115200
+#define SERIAL_BAUDRATE B9600
+//define SERIAL_IS_A_FILE
+//define SERIAL_IS_A_ATH0
