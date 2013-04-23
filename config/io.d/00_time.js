1507f6910dce9aec8badde797f3acdedd50e682e
32leaves <csweichel@googlemail.com>
2013-04-23 22:27:41 +0100

    Added a 5 minute signal

--- config/io.d/00_time.js
+++ config/io.d/00_time.js
@@ -10,6 +10,7 @@ var io_time = require(__lib + 'io_time.js');
  */
 module.exports = function(bus, modules) {
     return new io_time(bus)(function(io) {
-        //io.connect(60 * 1000, "time.minute.1");
+        io.connect(60 * 1000, "time.minute.1");
+        io.connect(60 * 1000, "time.minute.5");
     });
 };
